af4dc897c94cd1dba6e237fbfb78e6985b08c641
murtazaaylak <murtazaaylak@gmail.com>
2021-05-16 22:48:20 +0300

    custom routes added

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const express = require('express');
 
+const timestamp = require('./routes/api/timestamp');
 
-const app = epress();
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hi')
+})
+
+// Use routes
+app.use('/api/timestamp', timestamp);
+app.use('/api', timestamp);
+
+const port = process.env.PORT || 3000
+app.listen(port, () =>{
+    console.log(`Server started on port: ${port}`);
+});
---
+++ routes/api/timestamp.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/timestamp
+// @desc    brings current date in json format as 'unix' and 'utc'
+// @access  public
+
+router.get('/', (req, res) => {
+    // if there is no custom route, show the current date.
+    let dateObj = new Date();
+        res.json({
+            unix: dateObj.valueOf(),
+            utc: dateObj.toUTCString()
+        });
+})
+
+// @route   GET api/timestamp/:dateString
+// @desc    brings custom date in json format as 'unix' and 'utc'
+// @access  public
+
+router.get('/:customDate', (req, res) => {
+    // assign the route to a constant
+    const customDate = req.params.customDate;
+
+    // if there isn't a non-number character in the route, it is in unix format
+    if(/\D/.test(customDate) === false){
+        let unix = parseInt(customDate);
+        res.json({
+            unix,
+            utc: new Date(unix).toString()
+        })
+    } else {
+        let dateObj = new Date(customDate);
+        // if the route is not a valid date, we throw err
+        if(dateObj.toString() === 'Invalid Date'){
+            res.json({
+                err: 'Invalid Date'
+            })
+        } else {
+            res.json({
+                unix: dateObj.valueOf(),
+                utc: dateObj.toString()
+            })
+        }
+    }
+})
+
+module.exports = router;
